f84f7fea5acb9c7e30a290a83fc287004f1ae8d2
diegoholiveirav <diegoholiveirav@gmail.com>
2021-03-25 18:27:37 -0300

    Done Without Errors

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,6 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
-
     const newTask = {
       id: Math.random(),
       title: newTaskTitle,
